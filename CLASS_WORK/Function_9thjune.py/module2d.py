cb6e8777fc7b3798d6f243f841f94b2660ea5c52
Aniket Parwary <parwaryaniket@gmail.com>
2026-06-09 15:16:24 +0530

    first commit

---
+++ CLASS_WORK/Function_9thjune.py/module2d.py
@@ -0,0 +1,15 @@
+# Import the module
+import peramiter
+
+# Rectangle
+length = 10
+width = 5
+
+print("Rectangle Area =", peramiter.rectangle_area(length, width))
+print("Rectangle Perimeter =", peramiter.rectangle_perimeter(length, width))
+
+# Square
+side = 4
+
+print("Square Area =", peramiter.square_area(side))
+print("Square Perimeter =", peramiter.square_perimeter(side))
---
+++ CLASS_WORK/Function_9thjune.py/peramiter.py
@@ -0,0 +1,21 @@
+'''
+To create a module that has functions to calculate area and perimeter of 2d figures
+'''
+# Function to calculate area of rectangle
+def rectangle_area(length, width):
+    return length * width
+
+
+# Function to calculate perimeter of rectangle
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
+
+
+# Function to calculate area of square
+def square_area(side):
+    return side * side
+
+
+# Function to calculate perimeter of square
+def square_perimeter(side):
+    return 4 * side
